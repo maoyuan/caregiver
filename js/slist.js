12ea7d596daa2b8e5dcf30bb78649818a5229757
Maoyuan <smaoyuan@gmail.com>
2015-03-22 05:39:36 -0400

    update shopping list

--- js/slist.js
+++ js/slist.js
@@ -18,7 +18,20 @@ $("#slist_calendar_cancel").click(function() {
 	$("#slist_end_date").val("");
 });
 
+$('#slist_shoppling_list_table').hide();
 
 $("#slist_calendar_ok").click(function(){
+	// console.log($('#slist_start_calendar').data("DateTimePicker").date().format('MM-DD-YYYY'));
 
+	$.getJSON("./data.json", function(json) {
+		var htmlCode = "";
+		$("#slist_recipe_table").empty();
+		for (var i = 0; i < json.length; i++) {
+			var rname = json[i].name;
+			htmlCode += "<tr><td>" + (i + 1) + ". " + rname + "</td><tr/>";
+		}
+		$("#slist_recipe_table").append(htmlCode);
+
+		$('#slist_shoppling_list_table').show();
+	});
 });
