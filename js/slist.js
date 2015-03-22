71aa47c6a39c80f01791900107966b9f1f337bc3
Maoyuan <smaoyuan@gmail.com>
2015-03-22 04:18:07 -0400

    update the shopping list view

--- js/slist.js
+++ js/slist.js
@@ -1,4 +1,24 @@
 
-$('#slist_start_calendar').datetimepicker();
+$('#slist_start_calendar').datetimepicker({
+	format: 'ddd, MMM D YYYY',
+	// icons: {
+	// 	time: "fa fa-clock-o",
+	// 	date: "fa fa-calendar",
+	// 	up: "fa fa-arrow-up",
+	// 	down: "fa fa-arrow-down"
+	// }
+});
 
-$('#slist_end_calendar').datetimepicker();
+$('#slist_end_calendar').datetimepicker({format: 'ddd, MMM D YYYY'});
+
+
+// clear two calendar selection
+$("#slist_calendar_cancel").click(function() {
+	$("#slist_start_date").val("");
+	$("#slist_end_date").val("");
+});
+
+
+$("#slist_calendar_ok").click(function(){
+
+});
