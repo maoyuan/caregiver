fb8c64e9e7bdaa306103b8758b0eccd34be15c1d
Junyang <junyang@vt.edu>
2015-03-22 02:12:09 -0400

    edit index.html rsearch.js

--- js/rsearch.js
+++ js/rsearch.js
@@ -44,11 +44,15 @@ $(document).ready(function() {
         var keyword = ui.item.value;
         $.getJSON("./data.json", function(json) {
             $("table").empty();
+            $("#popup").empty();
             for(var i = 0; i < json.length; i++){
                 var imgname = "img/" + json[i].image;
                 var recipename = json[i].name;
                 var ingredients = json[i].ingredients;
+                var directions = json[i].directions;
+                var modalid = "modal" + (i+1);
                 if(ingredients.indexOf(keyword) > -1 || recipename.indexOf(keyword) > -1) {
+                    var ingreArr = ingredients.split("===");
                     var htmlcode = '<tr><td><div class="row"><div class="col-md-4">';
                     htmlcode += '<a href = "#"><img id="theImg" src="' +imgname+ '" width="140" height="140"/></a>';
                     htmlcode += '</div><div class="col-md-8">';
@@ -61,10 +65,33 @@ $(document).ready(function() {
                     htmlcode += '<i class="fa fa-star-o"></i></div></div>';
                     // htmlcode += '<span class="label label-info">Advanced</span>';
                     // htmlcode += '<span class="label label-warning">Long Time</span>';
-                    htmlcode += '<a class="btn btn-default" href="#">Detail Receipe <span class="glyphicon glyphicon-chevron-right"></span></a>';
+
+                    //htmlcode += '<a class="btn btn-default" href="#">Detail Receipe <span class="glyphicon glyphicon-chevron-right"></span></a>';
+                    htmlcode += '<p>'+ ingreArr[0] + '</p>';
+                    htmlcode += '<p>'+ ingreArr[1] +'</p>';
+                    htmlcode += '<p> ... </p>';
+                    htmlcode += '<button type="button" class="btn btn-default" data-toggle="modal" data-target="#' + modalid+'">Detail Receipe </button>';
+                    //htmlcode += '<input type = "text" class = "dp6" value = "" data-date-format = "mm-dd-yy" ></input>';
+                    htmlcode += '<button type="button" class="btn btn-default" data-toggle="modal" data-target="#calendar">Add to Favorite</button>';
+
                     htmlcode += '</div></td></tr>';
                     
                     $("#nutrition").append(htmlcode);
+                    
+                    var popcode = '<div class="modal fade" id="' + modalid +'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
+                    popcode += '<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">Receipe</h4></div><div class="modal-body"><div><h4>Ingredients</h4>';    
+                    
+                    for(e in ingreArr){
+                        popcode += '<p>' + ingreArr[e] + '</p>';
+                    }
+                    popcode += '<h4>Preparation</h4>';
+                    var preArr = directions.split("===");
+                    for(e in preArr){
+                        popcode += '<p>' + preArr[e] + '</p>';
+                    }             
+                    popcode += '</div></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Add to Favorite</button><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>';
+                    
+                    $("#popup").append(popcode);
                 }
 
                 $('.btn_rsearch_rating').on('click', function(){
@@ -77,9 +104,17 @@ $(document).ready(function() {
                 });
                 
             }
+            
+            $('.dp6').datepicker({
+                todayBtn: 'linked'
+            });
         
         });
             $("#nutrition").show();  
-        }
+        }        
     });
   });
+
+// datepicker
+
+  
